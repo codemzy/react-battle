afc39009e5a6abc58370e7d1ed465cf602c30d08
Emma J <codemzy@gmail.com>
2016-05-25 14:28:12 +0000

    Created nested props and passed profile data from an object

--- app/index.js
+++ app/index.js
@@ -1,14 +1,48 @@
-
+const USER_DATA = {
+  name: 'Codemzy',
+  username: 'codemzy',
+  image: 'https://pbs.twimg.com/profile_images/701827019198300160/im0uC1ma.jpg'
+};
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-class HelloWorld extends React.Component {
+class Avatar extends React.Component {
   render() {
     return (
-      <div> Hello {this.props.name}! </div>
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     );
   }
 }
 
-ReactDOM.render(<HelloWorld name="Emma" />, document.getElementById('app'));
+class ProfilePic extends React.Component {
+  render() {
+    return (
+      <img src={this.props.imageUrl} />
+    );
+  }
+}
+
+class ProfileLink extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>{this.props.username}</a>
+      </div>
+    );
+  }
+}
+
+class ProfileName extends React.Component {
+  render() {
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+}
+
+ReactDOM.render(<Avatar user={USER_DATA} />, document.getElementById('app'));
