1cc33ceffc4e950ca41ce6e3bc4366f477d86412
Emma J <codemzy@gmail.com>
2016-05-25 16:09:50 +0000

    Updated routes with hashHistory

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,6 +4,8 @@ var ReactRouter = require('react-router');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
+// use hashHistory which is default anyway
+var hashHistory = ReactRouter.hashHistory;
 
 // Components
 var Main = require('../components/Main');
@@ -11,7 +13,7 @@ var Home = require('../components/Home');
 
 // Routes
 var routes = (
-    <Router> 
+    <Router history={hashHistory}> 
         <Route path="/" component={Main}>
             <IndexRoute component={Home} />
             <Route path='/playerOne' component={Home} />
