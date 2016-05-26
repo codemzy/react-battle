53e4bbba7856ad5d996cccd8487ef9759729b283
Emma J <codemzy@gmail.com>
2016-05-26 17:03:43 +0000

    Refactored Prompt component to function as stateless

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -1,19 +1,13 @@
 var React = require('react');
 
-class Prompt extends React.Component {
-    
-    constructor(props) {
-        super(props);
-    }
-    
-    render() {
+function Prompt (props) {
         return (
           <div className='jumbotron col-sm-6 col-sm-offset-3 text-center'>
-            <h1>{this.props.header}</h1>
+            <h1>{props.header}</h1>
             <div className="col-sm-12">
-                <form onSubmit={this.props.submitUser.bind(this)}>
+                <form onSubmit={props.submitUser}>
                     <div className="form-group">
-                        <input className="form-control" placeholder="Github Username" type="text" value={this.props.username} onChange={this.props.updateUser.bind(this)} />
+                        <input className="form-control" placeholder="Github Username" type="text" value={props.username} onChange={props.updateUser} />
                     </div>
                     <div className="form-group col-sm-4 col-sm-offset-4">
                         <button className="btn btn-block btn-success" type="submit">Continue</button>
@@ -22,7 +16,6 @@ class Prompt extends React.Component {
             </div>
           </div>
         );
-    }
 }
 
 Prompt.propTypes = {
