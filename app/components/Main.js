a8daa89efe0123427537fb648abe827b0078ebc5
Emma J <codemzy@gmail.com>
2016-06-02 13:10:15 +0000

    Added css transition group and custom css passed via Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,14 @@
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+require('../main.css');
 
 class Main extends React.Component {
   render() {
     return (
       <div className='main-container'>
-        {this.props.children}
+        <ReactCSSTransitionGroup transitionName="appear" transitionEnterTimeout={500} transitionLeaveTimeout={500}>
+          {this.props.children}
+        </ReactCSSTransitionGroup>
       </div>
     );
   }
